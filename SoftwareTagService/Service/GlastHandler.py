0aa14767b8076cb54eeecbcb4745acf213149458
Stephane Poss <stephane.poss@cern.ch>
2013-04-05 16:28:09 +0200

    Renamed directory to match future service name

---
+++ SoftwareTagService/Service/GlastHandler.py
@@ -0,0 +1,36 @@
+"""
+Service code for GLAST stuff
+"""
+
+__RCSID__ = " $Id: $ "
+
+from DIRAC.Core.DISET.RequestHandler                    import RequestHandler
+from Glast.DB.GlastAdditionnalInfoDB                    import GlastAdditionnalInfoDB #Fix path
+from DIRAC import S_OK
+from types import StringTypes, DictType
+
+glastdb = False
+
+def initializeGlastHandler( ServiceInfo ):
+  global glastdb
+  glastdb = GlastAdditionnalInfoDB()
+  return S_OK()
+
+class GlastHandler(RequestHandler):
+
+  types_getSitesForTag = [StringTypes]
+  def export_getSitesForTag(self, tag):
+    return glastdb.getSitesForTag(tag)
+  
+  types_getTagsAtSite = [StringTypes]
+  def export_getTagsAtSite(self, site):
+    return glastdb.getTagsAtSite(site)
+  
+  types_addTagAtSite = [StringTypes, StringTypes]
+  def export_addTagAtSite(self, tag, site):
+    return glastdb.addTagAtSite( tag, site )
+  
+  types_removeTagAtSite = [StringTypes, StringTypes]
+  def export_removeTagAtSite(self, tag, site):
+    return glastdb.removeTagAtSite(tag, site)
+  
