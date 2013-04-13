f16dacaf1b7d61945b636de6d17cf1dae34ca2b7
Stephane Poss <stephposs@gmail.com>
2013-04-13 21:02:52 +0200

    massive update

--- SoftwareTagSystem/scripts/dirac-glast-tag-admin.py
+++ SoftwareTagSystem/scripts/dirac-glast-tag-admin.py
@@ -1,3 +1,4 @@
+#!/bin/env python
 """
 The user interface script for tag handling. Idea is to provide 4 methods
 dirac-glast-tag-admin addTag <tag>
@@ -6,48 +7,77 @@ dirac-glast-tag-admin flagOkay <tag>
 dirac-glast-tag-admin flagBad <tag>
 including the --site= parameter, it will only act on the site 
 """
-def setSpecialOption( optVal ):
-    from DIRAC import S_OK
-    global specialOptions
-    option,value = optVal.split('=')
-    specialOptions[option] = value
-    return S_OK()
+from DIRAC.Core.Base import Script
+from DIRAC import S_OK
 
+statuslist = ["add",'remove','flagOK','flagBad']
+
+class Params(object):
+    def __init__(self):
+        self.site = []
+        self.tag = ''
+        self.status = ""
+    def setSite(self,opt):
+        self.site = opt.split(',')
+        return S_OK()
+    def setTag(self,opt):
+        self.tag = opt
+        return S_OK()
+    def setStatus(self,opt):
+        if not opt in statuslist:
+            return S_ERROR("Status must be among %s" % statuslist)
+        self.status = opt
+        return S_OK()
+    def registerswitch(self):
+        Script.registerSwitch("T:", 'Tag=', 'Tag to affect', self.setTag)
+        Script.registerSwitch("S:", 'Sites=', 'Site(s) to consider, comma separated', self.setSite)
+        Script.registerSwitch("",'Status=','Status to change, among %s'%statuslist,self.setStatus)
+        Script.setUsageMessage("-T and --Status are mandatory")
+    
 if __name__ == "__main__":
-    from DIRAC.Core.Base import Script
-    from GlastDIRAC.SoftwareTagSystem.Client import SoftwareTagClient
-    specialOptions = {}
-    Script.registerSwitch( "opt:", "options=", "Special option (currently supported: site", setSpecialOption)
-        # thanks to Stephane for suggesting this fix!
+    cli_p = Params()
+    cli_p.registerswitch()
+    # thanks to Stephane for suggesting this fix!
     Script.parseCommandLine()
-        
-    args = Script.getPositionalArgs()
-    if len(args)==0:
-        raise S_ERROR("not allowed.")
+
+    from DIRAC import gLogger, exit as dexit
+    from DIRAC.Core.Security.ProxyInfo import getProxyInfo
+    res = getProxyInfo()
+    if not res['OK']:
+        gLogger.error("Bad proxy, or no proxy")
+        dexit(1)
+    group = res['Value']['group']
+    if not group == 'SomeDIRACgroup' :
+        gLogger.error('Invalid group, cannot proceed')
+        dexit(1)
+
+    from GlastDIRAC.SoftwareTagSystem.Client import SoftwareTagClient
+    
+    if not cli_p.tag or not cli_p.status:
+        Script.showHelp()
+        dexit(1)
     client = SoftwareTagClient()
     sites = []
-    if 'site' in specialOptions:
-        sites = [specialOptions['site']]
+    if cli_p.site:
+        sites = cli_p.site
     else:
         sites = client.getSites()
-    mode = args[0]
-    if not mode in ['addTag','removeTag','flagOkay','flagBad']:
-        raise S_ERROR("not supported")
-    else:
-        if len(args)<1:
-            raise S_ERROR("Need arg 2.")
-    tag = args[1]
-    if mode == "addTag":
+    mode = cli_p.status
+    
+    tag = cli_p.tag
+    if mode == "add":
         for site in sites:
             client.addTagAtSite(tag,site)
-    elif mode == "removeTag":
+    elif mode == "remove":
         for site in sites:
             client.removeTagAtSite(tag,site)
     elif mode.startswith("flag"):
         status = None
-        if mode == "flagOkay":
+        if mode == "flagOK":
             status = "Okay"
         else:
             status = "Bad"
         client.updateStatus(tag,site,status)
+
+    dexit(0)
     
