aaf2d171a944cef396686de9ae305e813885d146
zimmerst <zimmerst@gmail.com>
2013-10-22 16:45:23 +0200

    change setGroup to setJobGroup() - thanks SP

--- ResourceStatusSystem/scripts/dirac-glast-softwaretag-cli.py
+++ ResourceStatusSystem/scripts/dirac-glast-softwaretag-cli.py
@@ -123,21 +123,6 @@ class SoftwareTagCli(cmd.Cmd):
             print "Found errors, cannot continue"
             return
     
-    def do_listpreinstalled(self,argss):
-        """ List softwares pre-install on ccglast02.in2p3.fr. thoses software are available to be install on sites
-            listpreinstalled
-        """
-   
-        cmd_listpreinstall = "cat /glast/applicat/preinstallGlastRelease/listsoftware.list"
-        cmd_ssh = 'ssh glastpro@ccage.in2p3.fr "ssh ccglast02.in2p3.fr \\"'+cmd_preinstall+'\\" \" '
-   
-        print " - List of softwares pre installed on ccglast02.in2p3.fr available to be install on sites :"
-
-        if os.system(cmd_ssh) != 0 :
-            print "Error during the command '"+cmd_ssh+"'"
-            return
-
-    
     def do_reset(self,args):
         """ 
             to reset a tag system-wide use:
@@ -325,7 +310,7 @@ class SoftwareTagCli(cmd.Cmd):
         j.setDestination(site)
         j.setCPUTime(1000)
         j.setName("Installation "+tag)
-        j.setExecutable(os.environ['DIRAC']+"/GlastDIRAC/ResourceStatusSystem/Client/"+scriptname +" "+tag , logFile='SoftwareInstallation.log')
+        j.setExecutable(os.environ['DIRAC']+"/GlastDIRAC/ResourceStatusSystem/Client/"+scriptname , logFile='SoftwareInstallation.log')
         j.setOutputSandbox('*.log')
         res = d.submit(j)
         if not res['OK']:
