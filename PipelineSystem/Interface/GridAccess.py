baa0159306230f3ee68a38b02431c1fa7dba5b90
zimmer <zimmer@fysik.su.se>
2014-11-04 15:22:41 +0100

    catch weird KeyError, LPG-39

--- PipelineSystem/Interface/GridAccess.py
+++ PipelineSystem/Interface/GridAccess.py
@@ -9,9 +9,10 @@ Script.parseCommandLine( ignoreErrors = False )
 import DIRAC
 from DIRAC import gLogger, S_OK, S_ERROR
 import os, time
-from DIRAC.ConfigurationSystem.Client.Helpers.Operations import Operations
+from DIRAC.ConfigurationSystem.Client.Helpers.Operations    import Operations
 from DIRAC.Core.Security.ProxyInfo                          import getProxyInfo
 from DIRAC.DataManagementSystem.Client.ReplicaManager       import ReplicaManager
+from DIRAC.DataManagementSystem.Client.ReplicaManager       import CatalogFile
 from DIRAC.Core.Utilities.List                              import sortList
 
 # Set up message logging
@@ -23,22 +24,26 @@ class stageGrid(object):
     """
     
     def __init__(self,dataDir):
+        op = Operations("glast.org")
         self.stagingArea = dataDir
         self.listFileStaged = []    
         self.nbofSEtried =0
         self.userprefix = None
         self.prefixDest = None
         self.stagingDest = None
-        self.listSEs = os.environ['SEs_AVAILABLE'].split(',')
+        SEtemporaryStaging = op.getValue( 'Pipeline/StorageElementTemporaryStaging', "" )
+        self.listSEs = SEtemporaryStaging.split(',')
+    
         self.SE = None
-        self.log = gLogger.getSubLogger("GPL Staging")
-        if self.__getPrefix()!=0:
+        self.log = gLogger.getSubLogger("GridAccess")
+        if self.__getPrefix():
             self.log.error("Failed to initialize staging.")
             raise Exception("Failed to initialize!")
         self.__pickRandomSE()
-    
+    def setLogLevel(self,logLevel):
+        self.log.setLevel(logLevel)
     def __getPrefix(self):
-        op = Operations()
+        op = Operations("glast.org")
         self.userprefix = None
         res = getProxyInfo()
         if res['OK']:
@@ -55,10 +60,13 @@ class stageGrid(object):
             self.log.error("Could not find task category")
             return 1
 
-        self.prefixDest = op.getValue("Pipeline/StorageElementBasedDir",self.userprefix)
-        self.stagingDest = self.prefixDest+task_category+os.environ['PIPELINE_TASK']+"/"+os.environ['PIPELINE_STREAM']
+        self.prefixDest = op.getValue("Pipeline/StorageElementBasePath",self.userprefix)
+        self.stagingDest = self.prefixDest+"/"+task_category+"/"+os.environ['PIPELINE_TASK']+"/"+os.environ['PIPELINE_STREAM']
         return 0
-    
+   
+    def getDestinationDir(self):
+        return self.stagingDest
+ 
     def getStageDir(self):
         """
         @return: string - stage directory
@@ -76,7 +84,8 @@ class stageGrid(object):
             self.SE = choice(self.listSEs)
         except IndexError:
             self.SE = None
-        
+        self.log.info('__pickRandomSE -> self.SE :')
+        self.log.info(self.SE) 
             
     def stageOut(self,outfile):
         """
@@ -102,7 +111,7 @@ class stageGrid(object):
         rc = 0
         rm = ReplicaManager()
         for item in self.listFileStaged:   
-            #self.log.info("SE '"+self.SE+"' == : '"+str(self.SE == "False")+"'")
+            #print("SE '"+self.SE+"' == : '"+str(self.SE == "False")+"'")
             if not self.SE:
                 self.log.info("No SE available for '"+item[0]+"'")
                 rc+=1
@@ -111,27 +120,28 @@ class stageGrid(object):
                 self.log.info("Trying to store '"+item[0]+"' in SE : '"+self.SE+"' ...")
                 result = rm.putAndRegister( item[1], item[0], self.SE)
                 if not result['OK']:
-                    self.log.warning('ERROR %s' % ( result['Message'] ))
-                    self.listSEs.remove(self.SE) # make sure not to pick the same SE again.             
+                    self.log.info('ERROR %s' % ( result['Message'] ))
 
-        self.log.info("Wait 5sec before trying again...")
-        time.sleep(5)
-        result = rm.putAndRegister( item[1], item[0], self.SE)
-        if not result['OK']:
-            self.log.warning('ERROR %s' % ( result['Message'] ))
-            while   not result['OK'] :
-                self.__pickRandomSE()
-                if not self.SE:
-                    break
-                self.log.info("Trying with another SE : '"+self.SE+"' . In 5sec...")
-                time.sleep(5)
-                result = rm.putAndRegister( item[1], item[0], self.SE)
-                if result['OK']:
-                    self.log.info("file stored : '"+item[1]+"' in '"+self.SE+"'")
-                else:
-                    self.log.info("ERROR : failed to store the file '"+item[1]+"' ...")
-                    self.listSEs.remove(self.SE) # make sure not to pick the same SE again. 
-                    rc += 1
+                    self.log.info("Wait 5sec before trying again...")
+                    time.sleep(5)
+                    result = rm.putAndRegister( item[1], item[0], self.SE)
+                    if not result['OK']:
+                        self.log.info('ERROR %s' % ( result['Message'] ))
+                        while   not result['OK'] :
+                            self.listSEs.remove(self.SE) # make sure not to pick the same SE again.    
+                            self.__pickRandomSE()
+                            if not self.SE:
+                                rc+=1
+                                break
+                            self.log.info("Trying with another SE : '"+self.SE+"' . In 5sec...")
+                            time.sleep(5)
+                            result = rm.putAndRegister( item[1], item[0], self.SE)
+                            if result['OK']:
+                                self.log.info("file stored : '"+item[1]+"' in '"+self.SE+"'")
+                            else:
+                                self.log.error("ERROR : failed to store the file '"+item[1]+"' ...")
+                                rc += 1        
+
         return rc
 
     def dumpStagedFiles(self):
@@ -155,7 +165,7 @@ class stageGrid(object):
         cksums = {}
         # Compute checksums for all stagedOut files
         
-        self.log.info("Calculating 32-bit CRC checksums for stagedOut files")
+        print("Calculating 32-bit CRC checksums for stagedOut files")
         #print "Calculating 32-bit CRC checksums for stagedOut files"
         
         for stagee in self.listFileStaged:
@@ -166,21 +176,26 @@ class stageGrid(object):
                 foo = fd.read()             # Calculate checksum
                 rc = fd.close()
                 if rc != None:
-                    self.log.warning("Checksum error: return code =  "+str(rc)+" for file "+File)
+                    self.log.error("Checksum error: return code =  "+str(rc)+" for file "+File)
                     #print "Checksum error: return code =  "+str(rc)+" for file "+file
                 else:
                     cksumout = foo.split()
                     cksums[cksumout[2]] = [cksumout[0],cksumout[1]]
             else:
-                self.log.warning("Checksum error: file does not exist, "+File)
+                self.log.error("Checksum error: file does not exist, "+File)
         return cksums
 
-def getOutputData(baseDir):
+        
+        
+        
+def getOutputData(baseDir,logLevel="INFO"):
+    gLogger.setLevel(logLevel)
+    exitCode = 0    
     listOutputData = []
     res = getProxyInfo( False, False )
     if not res['OK']:
         gLogger.error( "Failed to get client proxy information.", res['Message'] )
-        DIRAC.exit( 2 )
+        DIRAC.exit( 71 )
         
 
     print  'Will search for files in %s' % baseDir
@@ -210,41 +225,83 @@ def getOutputData(baseDir):
             files = dirContents['Files'].keys()
     # ######################################################################################################## #
     # get file
-    listlfn_toremove = []
-    ntries = 3
-    for lfn in sortList( allFiles ):
-        success = False
-        while ntries:
-            print " - getting '"+lfn+"'... ",
-            result = rm.getFile( lfn )
-            if not result['OK']:
-                print 'ERROR %s' % ( result['Message'] )
-                print 'sleep for 10s and try again.'
-                time.sleep(10)
-            else:
-                success = True
-                listOutputData.append(result['Value']['Successful'][lfn])
-                listlfn_toremove.append(lfn)
-                print "OK"
-                break
-            ntries -= 1
-        if not success:
-            print 'ERROR could not get file after %i trials. Giving up :('%ntries
-            exitCode = 2
+    ntries = 5
+    # getFile supports bulk requests
+    files_to_transfer = sortList(allFiles)
+    successful_files = []
+    failed_files = []
+    while ntries>0:
+        if len(failed_files):
+            files_to_transfer = failed_files
+        gLogger.info("getting the following *list* of files %s"%str(files_to_transfer))
+        result = rm.getFile(files_to_transfer)
+        if not result["OK"]:
+            gLogger.error("Could not complete ReplicaManager request")
+            gLogger.error(str(result["Message"]))
+            gLogger.info('sleep for 10s and re-try')
+            time.sleep(10)
+            break 
+        # next is to check what files we got
+        successful_files = result["Value"]["Successful"]
+        failed_files = result["Value"]["Failed"]
+        if len(failed_files):
+            gLogger.info("Could not retrieve one or more files")
+            for s in successful_files:
+                files_to_transfer.remove(s)
+            for f in failed_files:
+                gLogger.verbose("could not retrieve: %s"%f)
+        else: 
+            break
+        ntries -= 1
+    if len(failed_files):
+        gLogger.error('ERROR could not get all files after %i trials. Giving up :('%ntries)
+        exitCode = 23
+
+    if exitCode:
+        return {"OK":False,"Message":"Failed to finish operations.","RC":exitCode}    
+    return S_OK(successful_files);
+       
+def removeOutputData(baseDir,logLevel="INFO"):
+    gLogger.setLevel(logLevel)
+    res = getProxyInfo( False, False )
+    if not res['OK']:
+        gLogger.error( "Failed to get client proxy information.", res['Message'] )
+        return {"OK":False,"Message":"Failed to get client proxy information: %s"%str(res['Message']),"RC":71}
+
     # ######################################################################################################## #
-    # remove files
-    print 'Will remove retrieved files' 
-    for lfntoremove in listlfn_toremove:
-        print " - removing : '"+lfntoremove+"'",
-    result = rm.removeFile(listlfn_toremove)      
+    rm = ReplicaManager()
+    try:
+        result = rm.cleanLogicalDirectory(baseDir);
+    except KeyError,ke:
+        return {"OK":False,"Message":"Caught key error, full stacktrace below\n%s"%str(ke),"RC":137}
+    print "Ignore the message about the file '"+baseDir+"dirac_directory'"
     if not result['OK']:
         print 'ERROR: %s' % (result['Message'] )
-        exitCode = 2
-    else :
-        failed_files = result['Value']['Failed']
-        for key in failed_files:
-            print "Failed file %s: %s"%(key,failed_files[key])
-    # ######################################################################################################## #
-    if exitCode:
-        return S_ERROR("Failed to finish operations.")
-    return S_OK(listOutputData);
+        return {"OK":False,"Message":"Cleanup failed : %s"%baseDir,"RC":37}
+    return S_OK(baseDir + " has been suppressed")
+
+    
+def cleanOldOutputData(baseDir,logLevel="INFO"):
+    gLogger.setLevel(logLevel)
+    res = getProxyInfo( False, False )
+    if not res['OK']:
+        gLogger.error( "Failed to get client proxy information.", res['Message'] )
+        return {"OK":False,"Message":"Failed to get client proxy information: %s"%str(res['Message']),"RC":71}
+
+    cf = CatalogFile()
+    result = cf.getCatalogExists(baseDir) 
+    
+    if not result['OK']:
+        print 'ERROR: %s' % (result['Message'] )
+        return {"OK":False,"Message":"Failed to access directory : %s"%baseDir,"RC":31}
+        
+    if result['Value']['Successful'][baseDir]:
+        res = removeOutputData(baseDir)
+        if not result['OK']:
+            return {"OK":False,"Message":"Failed to remove files %s"%result["Message"],"RC":41}
+        else:
+            return S_OK(baseDir + " has been supressed")
+        
+    return S_OK("No previous outputdata found.")
+    
+
