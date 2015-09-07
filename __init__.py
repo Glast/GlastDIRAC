c7bc51c41f32b38ffca742babc8feffb157f5b35
zimmer <zimmer@fysik.su.se>
2015-09-07 10:51:16 +0200

    remove SoftwareTagSystem (was resident in the scripts)

--- __init__.py
+++ __init__.py
@@ -9,8 +9,8 @@ __path__ = extend_path(__path__, __name__)
 # Define Version
 
 majorVersion = 1
-minorVersion = 3
-patchLevel   = 17
+minorVersion = 4
+patchLevel   = 0
 preVersion   = 0
     
 version      = "v%sr%s" % ( majorVersion, minorVersion )
